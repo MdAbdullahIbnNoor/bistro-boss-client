e68c0beefcb90cc427291ced25ebd42e09107c95
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-11-15 01:53:40 +0600

    authentication on process

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import logo from '../../../assets/logo.png';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { CgLogOut } from "react-icons/cg";
+import avater from '../../../assets/avater.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user, logOut } = useContext(AuthContext);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleLogout = () => {
+    logOut()
+      .then(res => console.log("logout successfull"))
+      .catch(err => console.log(err))
+  }
 
   return (
     <nav className="lg:fixed relative bg-opacity-30 bg-gray-800 shadow z-10 max-w-screen-2xl">
@@ -67,11 +77,10 @@ const Navbar = () => {
 
           {/* Mobile Menu open: "block", Menu closed: "hidden" */}
           <div
-            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${
-              isOpen
-                ? 'translate-x-0 opacity-100'
-                : 'opacity-0 -translate-x-full'
-            }`}
+            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${isOpen
+              ? 'translate-x-0 opacity-100'
+              : 'opacity-0 -translate-x-full'
+              }`}
           >
             <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-24 lg:w-3/5">
               <NavLink
@@ -93,57 +102,50 @@ const Navbar = () => {
                 Order
               </NavLink>
               <NavLink
-                to="menu"
+                to="login"
                 className="px-3 py-2 mx-3 mt-2 text-lg text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Menu
+                Login
               </NavLink>
               <NavLink
-                to="menu"
+                to="signup"
                 className="px-3 py-2 mx-3 mt-2 text-lg text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Menu
+                SignUp
               </NavLink>
-              
-             
+
+
             </div>
 
             <div className="flex items-center mt-4 lg:mt-0">
-              <button
-                className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
-                aria-label="show notifications"
-              >
-                <svg
-                  className="w-6 h-6"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+              {
+                user ? <button
+                  onClick={handleLogout}
+                  className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
+                  aria-label="show notifications"
                 >
-                  <path
-                    d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+                  <CgLogOut
+                    className="w-8 h-8"
+                    fill="none"
                   />
-                </svg>
-              </button>
+                </button> : ""
+              }
 
               <button
                 type="button"
                 className="flex items-center focus:outline-none"
                 aria-label="toggle profile dropdown"
               >
-                <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
+                <div className="w-10 h-10 overflow-hidden border-2 border-gray-400 rounded-full">
                   <img
-                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                    className="object-cover w-full h-full"
+                    src={user ? user?.photoURL : avater}
+                    className="object-cover w-9 h-9"
                     alt="avatar"
                   />
                 </div>
 
                 <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">
-                  Khatab wedaa
+                  {user?.displayName}
                 </h3>
               </button>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
 import './index.css'
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-2xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
-  </React.StrictMode>,
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-2xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode >
 )
